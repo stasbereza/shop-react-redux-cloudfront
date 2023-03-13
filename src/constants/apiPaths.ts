99b5e3188e8625d7043a17ac2888d4ed675dcea6
Stanislav <stanislav.bereza@spglobal.com>
2023-03-13 18:17:42 +0200

    integrate importProductsFile lambda endpoint for uploading file

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://v79gt3qobf.execute-api.eu-west-1.amazonaws.com",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://qu9zuj0cgh.execute-api.eu-west-1.amazonaws.com",
   bff: "https://v79gt3qobf.execute-api.eu-west-1.amazonaws.com",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
